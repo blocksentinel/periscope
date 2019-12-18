d00936b9c4019f108c758bd230ab6374b76db5bc
majordutch <354294+majordutch@users.noreply.github.com>
2019-12-17 19:14:28 -0800

    Remove unused settings

--- src/Api/Host/Infrastructure/Settings.cs
+++ src/Api/Host/Infrastructure/Settings.cs
@@ -5,25 +5,12 @@ namespace Cinder.Api.Host.Infrastructure
     public class Settings
     {
         public DatabaseSettings Database { get; set; } = new DatabaseSettings();
-        public NodeSettings Node { get; set; } = new NodeSettings();
-        public QueueSettings Queue { get; set; } = new QueueSettings();
 
         public class DatabaseSettings : IDatabaseSettings
         {
             public string ConnectionString { get; set; }
             public string Tag { get; set; }
             public string Locale { get; set; }
-        }
-
-        public class NodeSettings
-        {
-            public string RpcUrl { get; set; }
-        }
-
-        public class QueueSettings
-        {
-            public string ConnectionString { get; set; }
-            public string Name { get; set; }
         }
     }
 }
