93c63219bba0fd999974d8e40e7cd6d64923d0c9
majordutch <354294+majordutch@users.noreply.github.com>
2019-12-26 23:25:32 -0800

    Add endpoint and feature for GetSupply query

---
+++ src/Api/Application/Features/Stats/GetSupply.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Cinder.Data.Repositories;
+using Cinder.Extensions;
+using MediatR;
+
+namespace Cinder.Api.Application.Features.Stats
+{
+    public class GetSupply
+    {
+        public class Query : IRequest<Model> { }
+
+        public class Model
+        {
+            public decimal Supply { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Model>
+        {
+            private readonly IAddressRepository _addressRepository;
+
+            public Handler(IAddressRepository addressRepository)
+            {
+                _addressRepository = addressRepository;
+            }
+
+            public async Task<Model> Handle(Query request, CancellationToken cancellationToken)
+            {
+                decimal supply = await _addressRepository.GetSupply(cancellationToken).AnyContext();
+
+                return new Model {Supply = supply};
+            }
+        }
+    }
+}
