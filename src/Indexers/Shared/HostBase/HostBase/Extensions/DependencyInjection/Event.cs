57ab7331f2030f5c68114761c4eb9d3b989b6a3d
majordutch <354294+majordutch@users.noreply.github.com>
2019-12-30 22:43:34 -0800

    Wire up RabbitMQ bus

--- src/Indexers/Shared/HostBase/HostBase/Extensions/DependencyInjection/Event.cs
+++ src/Indexers/Shared/HostBase/HostBase/Extensions/DependencyInjection/Event.cs
@@ -1,5 +1,7 @@
-﻿using Foundatio.Messaging;
+﻿using Cinder.Indexers.HostBase;
+using Foundatio.Messaging;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 // ReSharper disable once CheckNamespace
 namespace Cinder.Extensions.DependencyInjection
@@ -8,7 +10,18 @@ namespace Cinder.Extensions.DependencyInjection
     {
         public static void AddEvents(this IServiceCollection services)
         {
-            services.AddSingleton<IMessageBus>(new InMemoryMessageBus());
+            services.AddSingleton<IMessageBus>(sp =>
+            {
+                IOptions<SettingsBase> options = sp.GetService<IOptions<SettingsBase>>();
+
+                return new RabbitMQMessageBus(new RabbitMQMessageBusOptions
+                {
+                    ConnectionString = options.Value.Bus.ConnectionString,
+                    IsSubscriptionQueueExclusive = false,
+                    SubscriptionQueueAutoDelete = false,
+                    SubscriptionQueueName = options.Value.Bus.QueueName
+                });
+            });
         }
     }
 }
