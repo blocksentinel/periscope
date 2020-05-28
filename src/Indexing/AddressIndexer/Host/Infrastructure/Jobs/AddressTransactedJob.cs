b34bf5872865c3f2ecc24a61361c76f11351bb92
majordutch <354294+majordutch@users.noreply.github.com>
2020-05-28 00:15:42 -0700

    Add memory cache to reduce unnecessary requests to data store

--- src/Indexing/AddressIndexer/Host/Infrastructure/Jobs/AddressTransactedJob.cs
+++ src/Indexing/AddressIndexer/Host/Infrastructure/Jobs/AddressTransactedJob.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Cinder.Data.Repositories;
 using Cinder.Documents;
 using Cinder.Extensions;
+using Foundatio.Caching;
 using Foundatio.Jobs;
 using Foundatio.Queues;
 using Microsoft.Extensions.Logging;
@@ -11,11 +12,13 @@ namespace Cinder.Indexing.AddressIndexer.Host.Infrastructure.Jobs
 {
     public class AddressTransactedJob : QueueJobBase<AddressTransactedWorkItem>
     {
+        private readonly InMemoryCacheClient _addressCache;
         private readonly IAddressRepository _addressRepository;
 
         public AddressTransactedJob(IQueue<AddressTransactedWorkItem> queue, ILoggerFactory loggerFactory,
             IAddressRepository addressRepository) : base(queue, loggerFactory)
         {
+            _addressCache = new InMemoryCacheClient(builder => builder.MaxItems(500));
             _addressRepository = addressRepository;
         }
 
@@ -23,14 +26,19 @@ namespace Cinder.Indexing.AddressIndexer.Host.Infrastructure.Jobs
         {
             string address = context.QueueEntry.Value.Address;
 
+            if (await _addressCache.ExistsAsync(address).AnyContext())
+            {
+                return JobResult.SuccessWithMessage($"Address {address} exists in cache, skipping");
+            }
+
             try
             {
-                if (await _addressRepository.AddressExists(address).AnyContext())
+                if (!await _addressRepository.AddressExists(address).AnyContext())
                 {
-                    return JobResult.Success;
+                    await _addressRepository.UpsertAddress(new CinderAddress {Hash = address, ForceRefresh = true}).AnyContext();
                 }
 
-                await _addressRepository.UpsertAddress(new CinderAddress {Hash = address, ForceRefresh = true}).AnyContext();
+                await _addressCache.AddAsync(address, string.Empty).AnyContext();
             }
             catch (Exception e)
             {
