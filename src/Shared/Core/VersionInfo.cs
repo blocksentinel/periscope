706a3b0f43dccd42c5e0bb1f0eb9eead1e2d1639
majordutch <354294+majordutch@users.noreply.github.com>
2020-10-26 19:27:51 -0700

    Version bump

--- src/Shared/Core/VersionInfo.cs
+++ src/Shared/Core/VersionInfo.cs
@@ -5,7 +5,7 @@ namespace Cinder.Core
 {
     public static class VersionInfo
     {
-        public const string Version = "1.8.0-rc.1";
+        public const string Version = "1.8.0-rc.2";
         public static DateTime BuildDate => new FileInfo(typeof(VersionInfo).Assembly.Location).LastWriteTime;
     }
 }
